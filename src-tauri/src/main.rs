324de33ec7eb572535783f1925015498601054e5
taiyou <hiromacha1116@icloud.com>
2023-08-25 22:32:43 +0900

    for all dir

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,49 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[macro_use]
+extern crate serde;
+
+use std::fs;
+
+#[derive(Serialize)]
+#[serde(tag = "type")]
+enum Entry {
+    #[serde(rename = "file")]
+    File { name: String, path: String },
+    #[serde(rename = "dir")]
+    Dir { name: String, path: String },
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_entries(path: &str) -> Result<Vec<Entry>, String> {
+    let entries = fs::read_dir(path).map_err(|e| format!("{}", e))?;
+
+    let res = entries
+        .filter_map(|entry| -> Option<Entry> {
+            let entry = entry.ok()?;
+            let name = entry.file_name().to_string_lossy().to_string();
+            let path = entry.path().to_string_lossy().to_string();
+            let type_ = entry.file_type().ok()?;
+
+            if type_.is_dir() {
+                Some(Entry::Dir { name, path })
+            } else if type_.is_file() {
+                Some(Entry::File { name, path })
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    Ok(res)
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_entries])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,74 @@
-import { useState } from "react";
-// import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import { useState, useEffect } from 'react';
+import ReactPlayer from 'react-player';
+import { homeDir } from '@tauri-apps/api/path';
+import { convertFileSrc } from '@tauri-apps/api/tauri';
+import { invoke } from '@tauri-apps/api';
 
-function App() {
-  const [name, setName] = useState("anoymous");
+type Entry = {
+  type: 'dir' | 'file';
+  name: string;
+  path: string;
+};
+
+type Entries = Array<Entry>;
+
+const App = () => {
+  const [src, setSrc] = useState<string | null>(null);
+  const [dir, setDir] = useState<string | null>(null);
+  const [player, setPlayer] = useState<JSX.Element | null>(null);
+  const [entries, setEntries] = useState<Entries | null>(null);
   
+  useEffect(() => {
+    (async () => {
+      const home = await homeDir();
+      setDir(home);
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      if (!src) {
+        return;
+      }
+
+      const url = convertFileSrc(src);
+      const player = <ReactPlayer url={url} controls={true} />;
+      setPlayer(player);
+    })();
+  }, [src]);
+
+  useEffect(() => {
+    (async () => {
+      const entries = await invoke<Entries>("get_entries", { path: dir })
+        .catch(err => {
+          console.error(err);
+          return null;
+        });
+
+      setEntries(entries);
+    })();
+  }, [dir]);
+
+  const entry_list = entries ? <ul>
+    {entries.map(entry => {
+      if (entry.type === "dir") {
+        return <li key={entry.path} onClick={() => setDir(entry.path)}>{entry.name}</li>;
+      } else {
+        return <li key={entry.path} onClick={() => setSrc(entry.path)}>{entry.name}</li>;
+      }
+    })}
+  </ul> : null;
 
   return (
     <>
-      <div>Hello, {name}</div>
-      <input type="text" value={name} onChange={e => setName(e.target.value)}/>
+      <h1>React Player</h1>
+      {player}
+      <br />
+      src: {src ?? '(not selected)'}
+      <br />
+      dir: {dir ?? ''}
+      <br />
+      {entry_list}
     </>
   );
 }
